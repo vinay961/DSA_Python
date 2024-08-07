a8bc1e00650a4537ff56954657c9e9d5d3488aa4
vinay961 <vinay100rai@gmail.com>
2024-08-07 16:20:34 +0530

    3sum

--- Array/interview.py
+++ Array/interview.py
@@ -224,10 +224,30 @@ def longest_consecutive_sequence(arr):
 # Find all unique triplets that gives sum zero
 def triplets(arr):
     if not arr:
-        return f"No any triplets exist for empty string."
-    n = len(arr)
+        return []
+    arr.sort()
     result = []
-    for i in range(n):
-        x = arr[i]
-        j,k = i+1,n-1
+    
+    for i in range(len(arr)-2):
+        if i>0 and arr[i] == arr[i-1]:
+            continue
+        left,right = i+1,len(arr)-1
+        while left<right:
+            total = arr[i]+arr[left]+arr[right]
+            
+            if total>0:
+                right -= 1
+            elif total<0:
+                left += 1
+            else:
+                result.append([arr[i],arr[left],arr[right]])
+                if left<right and arr[left] == arr[left+1]:
+                    left += 1
+                if left<right and arr[right] == arr[right-1]:
+                    right -= 1
+                left += 1
+                right -= 1
+
+    return result
+    
         
